ae0a1e58a0ac5639113d3eb49fd5e51b70e9a733
louissass <lsass@usgs.gov>
2017-06-22 15:11:05 -0800

    new paths

--- functions/calcStakeCatch.m
+++ functions/calcStakeCatch.m
@@ -1,78 +1,75 @@
-function [ ready,StakeCatch ] = calcStakeCatch( glacier )
+function [ ready, mb_params ] = calcStakeCatch( glacier )
 %findCatch calculates the relative catch efficiency for each stake
 %   
 dbstop if error
 
+%ins
 mb_data = readtable(['data/',glacier,'/Input/Input_',glacier,'_Glaciological_Data.csv']);
-wx_data = readtable(['data/',glacier,'/Output/Output_',glacier,'filledWx.csv']);
+wx_data = readtable(['data/',glacier,'/Intermediate/',glacier,'FilledWx.csv']);
 wx_meta = readtable(['data/',glacier,'/Input/Input_',glacier,'_Wx_MetaData.csv']);
-stakecatch=['data/',glacier,'/Output/Calibrated_',glacier,'_CatchEfficiency.csv']; 
+
+%outs
+out = (['data/',glacier,'/Intermediate/',glacier,'_CatchEfficiency.csv']); 
+
+
 lapse=-6.5; %moist adiabatic lapse rate 
 
-mb_data.date_1 = datetime(mb_data.date_1); %let matlab know we are using dates
-mb_data.date_2 = datetime(mb_data.date_2);
+mb_data.Prior_fall_date = datetime(mb_data.Prior_fall_date); %let matlab know we are using dates
+mb_data.Spring_date = datetime(mb_data.Spring_date);
+mb_data.Fall_date = datetime(mb_data.Fall_date);
 wx_data.date = datetime(wx_data.date);
 
-stakes = unique(mb_data.site_name);
+stakes = unique(mb_data.Name);
 mb_params = table;
-mb_params.bal_year = mb_data.bal_year;
-mb_params.site_name = mb_data.site_name;
+mb_params.Year = mb_data.Year;
+mb_params.Name = mb_data.Name;
 
 %% calculate the catch efficiency at each stake
 for n = 1:length(stakes)
-    stakedata = mb_data(strcmp(mb_data.site_name,stakes(n)),:);
-    priorFallDate = datetime(year(stakedata.date_1)-1,10,1);%initialize with october 1 of prior year
-    tempdate = [NaT;stakedata.date_2(1:end-1)];
-    priorFallDate(~isnat(tempdate)) = tempdate(~isnat(tempdate));%overwrite with prior fall measurement date on that stake
-    Z_offset = stakedata.z - wx_meta.Elevation;
+    stakedata = mb_data(strcmp(mb_data.Name,stakes(n)),:);
+    Z_offset = stakedata.Z - wx_meta.Elevation;
     T_offset = Z_offset*lapse/1000;
     for m = 1:height(stakedata)
-        start = find(wx_data.date==priorFallDate(m));
-        finish = find(wx_data.date==stakedata.date_1(m));
+        start = find(wx_data.date==stakedata.Prior_fall_date(m));
+        finish = find(wx_data.date==stakedata.Spring_date(m));
         PrecipGauge = wx_data.P(start:finish)/1000; %precip in the gauge during the measurement period, converted to m
         temp = wx_data.T(start:finish) + T_offset(m); %estimated temperatures at the stake 
         firstaccumulation = find(PrecipGauge>=0.0002 & temp<=2); %first snowfall at a particular stake...
         SnowEstimated(m) = sum(PrecipGauge(firstaccumulation:end)); %so we include the swe added from rain on snow, but not rain at the beginning of the season
-        SnowMeasured(m) = stakedata.winter(m) - stakedata.winter_ablation(m);
+        SnowMeasured(m) = stakedata.Winter_accumulation(m);
         clear PrecipGauge temp
     end
     tempCatch = SnowMeasured'./SnowEstimated';
     medCatch = nanmedian(tempCatch);
     tempCatch(~isfinite(tempCatch)) = medCatch; 
-    mb_params.Catch(strcmp(mb_data.site_name,stakes(n)),1) = tempCatch;
+    mb_params.Catch(strcmp(mb_data.Name,stakes(n)),1) = tempCatch;
     
     clear tempCatch medCatch SnowMeasured SnowEstimated 
 end
 
 %% calculate melt coefficients
 
-bw = mb_data.winter - mb_data.winter_ablation;
-bs = mb_data.net - (mb_data.summer_accumulation + bw);
-
-Z_offset = mb_data.z - wx_meta.Elevation;
+Z_offset = mb_data.Z - wx_meta.Elevation;
 T_offset = Z_offset*(lapse/1000);
-r = 1.50;
+r = 1.25;
 
-
-
-for n = 1:height(mb_data) %   NEED to add summer accumulation
-    start = find(wx_data.date==mb_data.date_1(n));
-    finish = find(wx_data.date==mb_data.date_2(n));
+for n = 1:height(mb_data) %   
+    start = find(wx_data.date==mb_data.Spring_date(n));
+    finish = find(wx_data.date==mb_data.Fall_date(n));
     summer_T = wx_data.T(start:finish) + T_offset(n);
     summer_P = wx_data.P(start:finish) * mb_params.Catch(n) / 1000;
     PDD_t(n) = sum(summer_T(summer_T>=0));
-    summer_snow(n) = sum(summer_P(summer_T<=2));
-    snow(n) = bw(n) + summer_snow(n);
-    melt(n) = bs(n) - summer_snow(n);
-    if -melt(n)<=snow
+    summer_snow(n) = sum(summer_P(summer_T<=0)); %this is the snow that the Wx station data implies fell in mid summer and melted away...
+    snow(n) = mb_data.Winter_accumulation(n) + mb_data.Pre_accumulation(n) + summer_snow(n);
+    melt(n) = mb_data.Annual_balance(n) - mb_data.Pre_accumulation(n) - mb_data.Winter_accumulation(n) - summer_snow(n);
+    if -melt(n)<=snow(n)
         ki(n) = NaN;
         ks(n) = melt(n)/PDD_t(n);
     else
         ki(n) = (snow(n) + melt(n) - snow(n)*r)/PDD_t(n);
         ks(n) = ki(n)/r;
     end
-    
-    
+       
 end
 figure(); hold on
 scatter(PDD_t, melt)
@@ -82,24 +79,31 @@ mb_params.k_s = ks';
 mb_params.k_i = ki';
  figure(); hold on
 for n = 1:length(stakes)
-    scatter(mb_params.bal_year(strcmp(mb_params.site_name,stakes(n))),mb_params.k_i(strcmp(mb_params.site_name,stakes(n))),'o');
-    scatter(mb_params.bal_year(strcmp(mb_params.site_name,stakes(n))),mb_params.k_s(strcmp(mb_params.site_name,stakes(n))),'+');
+    scatter(mb_params.Year(strcmp(mb_params.Name,stakes(n))),mb_params.k_i(strcmp(mb_params.Name,stakes(n))),'o');
+    scatter(mb_params.Year(strcmp(mb_params.Name,stakes(n))),mb_params.k_s(strcmp(mb_params.Name,stakes(n))),'+');
     
-    medki(n) = nanmedian(mb_params.k_i(strcmp(mb_params.site_name,stakes(n))));
-    medks(n) = nanmedian(mb_params.k_s(strcmp(mb_params.site_name,stakes(n))));
-    stdki(n) = nanstd(mb_params.k_i(strcmp(mb_params.site_name,stakes(n))));
-    stdks(n) = nanstd(mb_params.k_s(strcmp(mb_params.site_name,stakes(n))));
-    mb_params.k_i(strcmp(mb_params.site_name,stakes(n)) & ~isfinite(mb_params.k_i)) = medki(n);
-    mb_params.k_s(strcmp(mb_params.site_name,stakes(n)) & ~isfinite(mb_params.k_s)) = medks(n);
+    medki(n) = nanmedian(mb_params.k_i(strcmp(mb_params.Name,stakes(n))));
+    medks(n) = nanmedian(mb_params.k_s(strcmp(mb_params.Name,stakes(n))));
+    stdki(n) = nanstd(mb_params.k_i(strcmp(mb_params.Name,stakes(n))));
+    stdks(n) = nanstd(mb_params.k_s(strcmp(mb_params.Name,stakes(n))));
+    mb_params.k_i(strcmp(mb_params.Name,stakes(n)) & ~isfinite(mb_params.k_i)) = medki(n);
+    mb_params.k_s(strcmp(mb_params.Name,stakes(n)) & ~isfinite(mb_params.k_s)) = medks(n);
     
 end    
 
 ki_metric = nanmean(stdki);
 ks_metric = nanmean(stdks);
 
-if sum(isnan(StakeCatch))==0
-    ready = 1;
-    
+figure(); hold on
+years = unique(mb_data.Year);
+for n = 1:length(years);
+    plot(mb_data.Z(mb_data.Year == years(n)),mb_data.Annual_balance(mb_data.Year == years(n))); hold on
+end
+
+writetable(mb_params,out);
+
+if sum(isnan(mb_params.Catch))==0
+    ready = 1;   
 end
 
 
